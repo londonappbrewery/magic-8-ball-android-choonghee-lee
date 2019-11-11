1c9571c40ce286ce3f5a199ab9b6a5e04410910e
Choonghee Lee <choonghee.lee@hotmail.com>
2019-11-11 12:20:33 +0900

    Step 7: Link the Layout to Java Code
    Write the Java code to show a random answer

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,13 +1,37 @@
 package com.londonappbrewery.magiceightball;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    int[] ballImages = {
+            R.drawable.ball1, R.drawable.ball2, R.drawable.ball3,
+            R.drawable.ball4, R.drawable.ball5
+    };
+
+    ImageView ballDisplay;
+    Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ballDisplay = findViewById(R.id.image_eightBall);
+        myButton = findViewById(R.id.askButton);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumberGenerator = new Random();
+                int num = randomNumberGenerator.nextInt(5);
+                ballDisplay.setImageResource(ballImages[num]);
+            }
+        });
     }
 }
